bf5369ace1538f5ef88a1165c019161cd8a8763e
fixedevelloper <fixedevelloper@gmail.com>
2026-05-31 10:47:31 +0100

    ert

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000','https://guenunion.cscreativ.com/'], // L'URL de votre Next.js
+    'allowed_origins' => ['http://localhost:3000','https://guenunion.cscreativ.com'], // L'URL de votre Next.js
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
